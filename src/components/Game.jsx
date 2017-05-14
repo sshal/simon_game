b1443587f38daa0c29daa937cc2950c35ee5853e
Shalimov Sergey <shalimovser90@gmail.com>
2017-05-14 12:50:39 +0300

    Initial commit

---
+++ src/components/Game.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <p>Example</p>
+        )
+    }
+}
+
+module.exports = Game;
